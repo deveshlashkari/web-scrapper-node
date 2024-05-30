d4107ff2305f4e6fe95c472700df755e7c463d3b
Devesh Lashkari <deveshlashkari@gmail.com>
2024-05-30 16:40:14 +0530

    remove / route

--- src/app.js
+++ src/app.js
@@ -4,11 +4,7 @@ const scrapeRouter = require("./routes/scrape");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-app.use("/", (req, res) => {
-  res.send(
-    "Web Scrapper is for bots. To use from scripts and apps, please check github. Thank you!"
-  );
-});
+
 app.use("/api", scrapeRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
