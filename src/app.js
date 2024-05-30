7d0ed7f035a25ae996c0d7dbe983ece1ba74c680
Devesh Lashkari <deveshlashkari@gmail.com>
2024-05-30 12:47:12 +0530

    add env

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const scrapeRouter = require("./routes/scrape");
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api", scrapeRouter);
 app.listen(PORT, () => {
